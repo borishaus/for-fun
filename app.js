a159f127a1dc484e521f154516b5e7e19cb05af7
borishaus <boris.a.clark@icloud.com>
2024-05-19 17:42:43 -0400

    savign my work

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const hbs = require('hbs');
 var app = express();
 
+const hbs = require('hbs');
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
